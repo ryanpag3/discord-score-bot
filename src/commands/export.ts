70bbeabbfecc2854d05c1e005a744a6132dd177b
Ryan Page <ryanapage@gmail.com>
2020-09-28 07:30:44 -0700

    finalize export implementation, allow toggle between channel vs else

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -1,23 +1,56 @@
 import { Message } from 'discord.js';
 import { promises } from 'fs';
+import { Op } from 'sequelize';
 import BotType from '../constant/bot-type';
 import ScoreType from '../constant/score-type';
 import { User, Score, Scoreboard } from '../models';
-import { getMessageEmbed, parseArgs } from '../util/command';
+import { getMessageEmbed, getScoreType, parseArgs } from '../util/command';
 import scoreboard from './scoreboard';
 
 const exportCmd = async (user: User, command: string, message: Message) => {
+    const args = parseArgs(message);
+
+    if (args.length > 1) {
+        throw new Error(`Only one argument can be provided.`);
+    }
+    let type = ScoreType.SERVER;
+    const where = {
+        type: {
+            [Op.or]: [
+                ScoreType.SERVER,
+                ScoreType.SCOREBOARD
+            ]
+        },
+        serverId: message.guild.id
+    };
+
+    if (args.includes('c')) {
+        type = ScoreType.CHANNEL;
+        where['channelId'] = message.channel.id;
+        where.type[Op.or] = [ScoreType.CHANNEL];
+    }
 
     message.channel.startTyping();
 
     let data = await Score.findAll({
-        where: {
-            serverId: message.guild.id
+        where,
+        include: {
+            model: Scoreboard,
+            attributes: {
+                exclude: [
+                    'id',
+                    'serverId',
+                    'channelId'
+                ]
+            }
         },
-        include: Scoreboard,
         order: [['type', 'DESC']],
         attributes: {
-            exclude: ['id']
+            exclude: [
+                'id',
+                'serverId',
+                'channelId'
+            ]
         }
     });
 
@@ -29,12 +62,14 @@ const exportCmd = async (user: User, command: string, message: Message) => {
     const filepath = `/tmp/scorebot-${message.author.id}-${new Date().getTime()}.json`;
     await promises.writeFile(filepath, JSON.stringify(d, null, 4), 'utf-8');
     const embed = getMessageEmbed(message.author)
+        .setTitle(`${getScoreType(type)} Export Completed`)
         .setDescription(`
-        ${data.length} entries exported.
+        ${data.length} scores exported.
         `);
     embed.files = [filepath];
-    message.channel.send(embed);
+    await message.channel.send(embed);
     message.channel.stopTyping();
+    await promises.unlink(filepath);
 }
 
 export default exportCmd;
