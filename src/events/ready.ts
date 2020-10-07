b235c5acdd44fe2016391ab28b2a0e3486889d87
Ryan Page <ryanapage@gmail.com>
2020-10-07 12:30:26 -0700

    set bot presence on ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
 import logger from '../util/logger'
 import { keywordsInitialized, loadKeywords } from '../util/keyword';
 import { loadUserScores } from '../util/user-score';
+import discordClient from '../util/discord-client';
 
 export default async () => {
     await loadKeywords();
@@ -10,4 +11,11 @@ export default async () => {
         throw new Error(`Keywords must be initialized before bot startup.`);
     
     logger.info(`Score Bot has successfully connected to Discord.`);
+    discordClient.user.setPresence({
+        status: 'online',
+        activity: {
+            name: '.sb help'
+        }
+    })
+
 }
