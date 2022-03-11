46a1f1806daa029a383c08de7254d51040ab5a51
Ryan Page <ryanapage@gmail.com>
2022-03-11 12:29:55 -0800

    export server info on request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,44 @@
-
+require('dotenv').config();
 import logger from '../util/logger';
-import { Score } from '../models';
+import { 
+    Score,
+    Scoreboard,
+    Keyword
+} from '../models';
 
 const fastify = require('fastify')();
 
-fastify.get('/migrate', async (request, reply) => {
-    const scores = await Score.findAll({
-        where: {}
-    });
+fastify.get('/export', async (request, reply) => {
+    try {
+        if (request.headers.authorization !== process.env.MIGRATE_SECRET)
+            return reply.code(401).send();
 
-    return JSON.stringify(scores, null, 4);
+        const scoreboards = await Scoreboard.findAll({
+            where: {
+                serverId: request.query.serverId
+            }
+        });
+
+        const scores = await Score.findAll({
+            where: {
+                serverId: request.query.serverId
+            }
+        });
+
+        const keywords = await Keyword.findAll({
+            where: {
+                serverId: request.query.serverId
+            }
+        });
+
+        return reply.type('application/json').send(JSON.stringify({
+            scores,
+            scoreboards,
+            keywords
+        }, null, 4));
+    } catch (e) {
+        logger.error(e);
+    }
 });
 
 export const startApi = async () => {
--- src/setup-bot.ts
+++ src/setup-bot.ts
@@ -1,15 +1,10 @@
 import { Client } from "discord.js";
 import setupEvents from './events';
-import env from './util/env';
 import runMigration from './util/umzug';
-import { loadKeywords } from './util/keyword';
-import { loadUserScores } from './util/user-score';
-import { startApi } from './api';
 
 const setupApp =  async (client: Client) => {
     await runMigration();    
     setupEvents(client);
-    await startApi();
 }
 
 export default setupApp;
